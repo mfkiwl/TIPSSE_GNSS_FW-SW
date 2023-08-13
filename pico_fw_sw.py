e0b52c7907a6e623880b7ee0655edacbccfebd4d
tclin0122 <F44061050@gs.ncku.edu.tw>
2023-08-13 19:19:55 +0800

    updating IQdata asm

--- pico_fw_sw.py
+++ pico_fw_sw.py
@@ -1,4 +1,13 @@
 from picozero import  pico_led
+import rp2
+from machine import Pin
 
 #make the led of pico as our board status check
 pico_led.on()
+
+#define the PIO with asm
+@rp2.asm_pio(set_init=rp2.PIO.IN_LOW) #initialize the PIO
+#define the IQ data function for PIO
+def IQdata():
+    wrap_target()
+    
