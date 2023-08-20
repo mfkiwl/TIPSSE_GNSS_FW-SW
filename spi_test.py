3218e6ef1d9b363b8d76eb5f155c7fde32e8aae0
tclin0122 <F44061050@gs.ncku.edu.tw>
2023-08-21 00:25:55 +0800

    add comments

--- spi_test.py
+++ spi_test.py
@@ -2,32 +2,39 @@ from picozero import pico_led
 from machine import Pin, SPI
 from time import sleep
 
-pico_led.on()
+pico_led.on() # check pico status
 
+# Pin define
+# chip open closed pin define
 shdn = Pin(4, Pin.OUT)
-
+# SPI pin define
 csn = Pin(1, Pin.OUT)
 mosi = Pin(3)
 spi = SPI(0, baudrate=1_000_000, polarity=0, phase=0, bits=8, sck=Pin(2), mosi=mosi, miso=Pin(0))
 
-shdn.high()
 
+
+#init chip
+shdn.high() #Open the chip
+
+# first time write STRMEN
 csn.low()
-spi.write(b'\x00\x20')
-spi.write(b'\x0e\xaf\xa9\xdc')
+spi.write(b'\x00\x20') #write value of config 3 (0x02)
+spi.write(b'\x0e\xaf\xa9\xdc')  #write STRMEN
+csn.high()
+
+sleep(0.1)
+
+# second time write STRMSTART
+csn.low()
+spi.write(b'\x00\x20') #write value of config 3 (0x02)
+spi.write(b'\x0e\xaf\xad\xdc') #write STRMSTART
 csn.high()
 
 sleep(0.1)
 
 csn.low()
-spi.write(b'\x00\x20')
-spi.write(b'\x0e\xaf\xad\xdc')
-csn.high()
-
-sleep(0.1)
-
-csn.low()
-spi.write(b'\x00\x28')
+spi.write(b'\x00\x28') #read value of config 3 (0x02)
 mosi.init(mode=Pin.IN)
 result = spi.read(4)
 spi = SPI(0, baudrate=1_000_000, polarity=0, phase=0, bits=8, sck=Pin(2), mosi=mosi, miso=Pin(0))
