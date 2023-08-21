629a8c1922320a0fd972de070cefcb1ba889f579
En Shih <seanstone5923@gmail.com>
2023-08-21 23:55:08 +0800

    pio blink

--- reference_example/hello/main.c
+++ reference_example/hello/main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
+#include <hardware/pio.h>
+#include "hello.pio.h"
+
+static inline void hello_program_init(PIO pio, uint sm, uint offset, uint pin)
+{
+    pio_sm_config c = hello_program_get_default_config(offset);
+
+    // Map the state machine's OUT pin group to one pin, namely the `pin`
+    // parameter to this function.
+    sm_config_set_out_pins(&c, pin, 1);
+    // Set this pin's GPIO function (connect PIO to the pad)
+    pio_gpio_init(pio, pin);
+    // Set the pin direction to output at the PIO
+    pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, true);
+
+    // Load our configuration, and jump to the start of the program
+    pio_sm_init(pio, sm, offset, &c);
+    // Set the state machine running
+    pio_sm_set_enabled(pio, sm, true);
+}
 
 int main()
 {
@@ -7,12 +27,27 @@ int main()
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    PIO pio = pio0;
+    // Our assembled program needs to be loaded into this PIO's instruction
+    // memory. This SDK function will find a location (offset) in the
+    // instruction memory where there is enough space for our program. We need
+    // to remember this location!
+    uint offset = pio_add_program(pio, &hello_program);
+
+    // Find a free state machine on our chosen PIO (erroring if there are
+    // none). Configure it to run our program, and start it, using the
+    // helper function we included in our .pio file.
+    uint sm = pio_claim_unused_sm(pio, true);
+    hello_program_init(pio, sm, offset, PICO_DEFAULT_LED_PIN);
+
     stdio_init_all();
     while (true) {
         printf("Hello, world!\n");
-        gpio_put(LED_PIN, 1);
+        // gpio_put(LED_PIN, 1);
+        pio_sm_put_blocking(pio, sm, 1);
         sleep_ms(500);
-        gpio_put(LED_PIN, 0);
+        // gpio_put(LED_PIN, 0);
+        pio_sm_put_blocking(pio, sm, 0);
         sleep_ms(500);
     }
 }
