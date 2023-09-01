b611f56f436e13f46374b4608f4e45e000f74c52
En Shih <seanstone5923@gmail.com>
2023-09-02 01:05:47 +0800

    Merge net.c into server.c

--- server.h
+++ server.h
@@ -11,4 +11,10 @@ void blink_cb(void *arg);
 size_t usb_tx(const void *buf, size_t len, struct mg_tcpip_if *ifp);
 bool usb_up(struct mg_tcpip_if *ifp);
 
+#if !defined(HTTP_URL)
+#define HTTP_URL "http://0.0.0.0:8000"
+#endif
+
+void web_init(struct mg_mgr *mgr);
+
 #endif
